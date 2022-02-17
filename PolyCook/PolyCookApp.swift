f48b3aca495a78dd93fbd357533b4370992c29db
Vivien LEVACHER <levacher.vivien@gmail.com>
2022-02-17 13:44:54 +0100

    Initial Commit

---
+++ PolyCook/PolyCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolyCookApp.swift
+//  PolyCook
+//
+//  Created by Vivien Levacher on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PolyCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
